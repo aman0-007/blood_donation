758d83200179fe0440f3b848011fb69e28643d77
aman0-007 <dwivedi.aman2811@gmail.com>
2024-07-18 16:04:36 +0530

    Merge remote-tracking branch 'origin/master'

--- lib/googlemap/getlocation.dart
+++ lib/googlemap/getlocation.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:geocoding/geocoding.dart';
 
-
-class Getlocation extends StatefulWidget {
-  const Getlocation({Key? key}) : super(key: key);
+class GetLocation extends StatefulWidget {
+  const GetLocation({Key? key}) : super(key: key);
 
   @override
-  State<Getlocation> createState() => _GetlocationState();
+  _GetLocationState createState() => _GetLocationState();
 }
 
-class _GetlocationState extends State<Getlocation> {
+class _GetLocationState extends State<GetLocation> {
   late GoogleMapController mapController;
   LatLng _center = const LatLng(45.521563, -122.677433); // Default center
   LatLng _lastMapPosition = const LatLng(45.521563, -122.677433); // Default position
   String _address = ""; // Variable to store address
+
+  Set<Marker> _markers = {}; // Set of markers for Google Maps
 
   @override
   void initState() {
@@ -24,19 +25,28 @@ class _GetlocationState extends State<Getlocation> {
   }
 
   void _getCurrentLocation() async {
-    Position position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
-    );
-    List<Placemark> placemarks = await placemarkFromCoordinates(
-      position.latitude,
-      position.longitude,
-    );
-    Placemark placemark = placemarks[0];
-    setState(() {
-      _center = LatLng(position.latitude, position.longitude);
-      _lastMapPosition = LatLng(position.latitude, position.longitude);
-      _address = "${placemark.name}, ${placemark.locality}, ${placemark.postalCode}, ${placemark.country}";
-    });
+    try {
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
+
+      if (placemarks != null && placemarks.isNotEmpty) {
+        Placemark placemark = placemarks[0];
+        setState(() {
+          _center = LatLng(position.latitude, position.longitude);
+          _lastMapPosition = LatLng(position.latitude, position.longitude);
+          _address =
+          "${placemark.name}, ${placemark.subLocality}, ${placemark.locality}, ${placemark.administrativeArea} ${placemark.postalCode}, ${placemark.country}";
+        });
+      }
+    } catch (e) {
+      print("Error getting location: $e");
+    }
   }
 
   void _onMapCreated(GoogleMapController controller) {
@@ -47,18 +57,80 @@ class _GetlocationState extends State<Getlocation> {
 
   void _onCameraMove(CameraPosition position) {
     _lastMapPosition = position.target;
+
+    // Update marker position to be centered on screen
+    setState(() {
+      _markers.clear();
+      _markers.add(
+        Marker(
+          markerId: MarkerId(_lastMapPosition.toString()),
+          position: _lastMapPosition,
+          draggable: true,
+          infoWindow: InfoWindow(
+            title: "Selected Location",
+            snippet: "Lat: ${_lastMapPosition.latitude}, Lng: ${_lastMapPosition.longitude}",
+          ),
+          onDragEnd: _onMarkerDragEnd,
+        ),
+      );
+    });
+
+    // Optional: Reverse geocode to update address based on new marker position
+    _getAddressFromLatLng(_lastMapPosition);
+  }
+
+  void _onMarkerDragEnd(LatLng position) {
+    _onCameraMove(CameraPosition(target: position, zoom: 15.0));
+  }
+
+  void _getAddressFromLatLng(LatLng position) async {
+    try {
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        position.latitude,
+        position.longitude,
+      );
+
+      if (placemarks != null && placemarks.isNotEmpty) {
+        Placemark placemark = placemarks[0];
+
+        String placeName = placemark.name ?? "Unknown Place";
+        String thoroughfare = placemark.thoroughfare ?? "";
+        String subThoroughfare = placemark.subThoroughfare ?? "";
+        String locality = placemark.locality ?? "";
+        String administrativeArea = placemark.administrativeArea ?? "";
+        String postalCode = placemark.postalCode ?? "";
+        String country = placemark.country ?? "";
+
+        String title = placeName.isNotEmpty
+            ? placeName
+            : '${thoroughfare.isNotEmpty ? thoroughfare : ''} ${subThoroughfare.isNotEmpty ? subThoroughfare : ''}';
+
+        String address = '';
+        if (placeName.isNotEmpty) {
+          address += '$placeName, ';
+        }
+        address +=
+        '$thoroughfare $subThoroughfare, $locality, $administrativeArea $postalCode, $country';
+
+        setState(() {
+          _address = address;
+        });
+      }
+    } catch (e) {
+      print("Error getting location: $e");
+    }
   }
 
   void _saveLocation() {
     print('Saved location: $_lastMapPosition');
-    print('Address: $_address'); // Print the address when saved
+    print('Address: $_address');
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Get Location'),
+        title: Text('Select Location'),
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.save),
@@ -76,6 +148,10 @@ class _GetlocationState extends State<Getlocation> {
         myLocationEnabled: true,
         compassEnabled: true,
         zoomControlsEnabled: false,
+        markers: _markers,
+        onTap: (LatLng position) {
+          _onMarkerDragEnd(position);
+        },
       ),
     );
   }
